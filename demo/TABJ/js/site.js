87c507b1d290af61d8afa917290315dd600fcc47
Eric Hsiao <eric00826@gmail.com>
2016-08-04 06:00:23 +0800

    .find('a')

--- demo/TABJ/js/site.js
+++ demo/TABJ/js/site.js
@@ -15,22 +15,22 @@ $(function(){
 
   TweenMax.to($(".ani"),.4 ,{x:'5',ease:Linear.easeOut ,yoyo:true ,repeat:-1});
 
-  ('.lists .btn a').mouseover(function(event) {
+  ('.lists .btn').find('a').mouseover(function(event) {
     $(this).find('.ani').css('display', 'none');
     $(this).find('.over').css('display', 'inline');
   });
 
-  $('.lists .btn a').mouseout(function(event) {
+  $('.lists .btn').find('a').mouseout(function(event) {
     $(this).find('.ani').css('display', 'inline');
     $(this).find('.over').css('display', 'none');
   });
 
-  $('.sub1 a').mouseover(function(event) {
+  $('.sub1').find('a').mouseover(function(event) {
     $(this).find('.ani').css('display', 'none');
     $(this).find('.over').css('display', 'inline');
   });
 
-  $('.sub1 a').mouseout(function(event) {
+  $('.sub1').find('a').mouseout(function(event) {
     $(this).find('.ani').css('display', 'inline');
     $(this).find('.over').css('display', 'none');
   });

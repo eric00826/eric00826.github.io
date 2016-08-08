9fac54d7bf9e25acf25816e553f2f4de6bf42177
Eric Hsiao <eric00826@gmail.com>
2016-08-08 18:01:42 +0800

    search

--- demo/Note7/js/main.js
+++ demo/Note7/js/main.js
@@ -7,11 +7,27 @@
 main = function (){
 
 	//private menbers
-
+	var isMenu = false;
 
 	//private methods
 	function init () {
 		console.log('main is loaded.');
+
+		initMenu();
+	}
+
+	function initMenu() {
+		$('.content-menu-open').click(function(event) {
+			if(!isMenu){
+				isMenu = true;
+				$('#content-menu').css('display', 'block');
+				$('.content-menu-open').css('background-image', 'url(images/mobile/menu-close.png)');
+			}else{
+				isMenu = false;
+				$('#content-menu').css('display', 'none');
+				$('.content-menu-open').css('background-image', '');
+			}
+		});
 	}
 
 	//constructor

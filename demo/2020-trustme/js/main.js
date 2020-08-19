bccb1a5010272511f217ba8490637b321ad83244
Eric Hsiao <eric00826@gmail.com>
2020-08-19 11:57:18 +0800

    .

--- demo/2020-trustme/js/main.js
+++ demo/2020-trustme/js/main.js
@@ -21,7 +21,9 @@ main = function () {
     var mySwiper = new Swiper('.swiper-container', {
       // Optional parameters      
       loop: true,
-
+      autoplay: {
+        delay: 5000,
+      },
       // If we need pagination
       pagination: {
         el: '.swiper-pagination',
